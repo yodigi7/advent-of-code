c35e9697f2162209329a56b1b88d18fd21a60374
Anthony Buchholz <anthony.buchholz@gmail.com>
2025-02-17 22:04:14 -0600

    add start of day24b 2024

--- 2024/day24/day24b.go
+++ 2024/day24/day24b.go
@@ -139,6 +139,40 @@ func solve(vals map[string]bool, equations []Equation) int {
 	return calcZ(vals)
 }
 
+// Returns whether the equation is currently touching an important node (z08, z19, z20...)
+// or somewhere down the line
+func getImportantEquations(eqs []Equation) []Equation {
+	var importantEqs []Equation
+	// Change this to be the important bits from your input
+	isImportant := map[string]bool{
+		"z38": true,
+		"z27": true,
+		"z26": true,
+		"z25": true,
+		"z24": true,
+		"z13": true,
+		"z12": true,
+		"z11": true,
+	}
+	added := true
+	firstAdd := true
+	for added {
+		added = false
+		for _, eq := range eqs {
+			resImp, okRes := isImportant[eq.result]
+			if resImp && (!okRes || firstAdd) {
+				importantEqs = append(importantEqs, eq)
+				isImportant[eq.result] = true
+				isImportant[eq.operand1] = true
+				isImportant[eq.operand2] = true
+				added = true
+			}
+		}
+		firstAdd = false
+	}
+	return importantEqs
+}
+
 func main() {
 	defer utils.Timeit("day 24")()
 	input, err := utils.ReadFileLines("day24/day24.txt")
@@ -146,6 +180,18 @@ func main() {
 		fmt.Println(err)
 	}
 	vals, equations := parse(input)
+	fmt.Println("Check the bits to see which positions are likely important")
 	fmt.Println(toBin(getExpectedZ(vals)))
 	fmt.Println(toBin(solve(vals, equations)))
+	fmt.Println("The equations we most likely need to swap:", getImportantEquations(equations))
 }
+
+// Chars 7-8, 17-22, 32-35
+// z46-7 = z39
+// 7-8 = z39-z38
+// 17-22 = z29-z24
+// 32-35 = z14-z11
+// Likely to just be 8, 19, 20, 21, 22, 33, 34, 35 because of carrying when adding
+// only swap nodes that are touching these
+// 1010110111011010010111101010000100011011100110
+// 1010111011011010101000101010000011111011100110
